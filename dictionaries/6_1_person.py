4ff5d9d71a33063a81e7a075db7325217a353603
davidcxpardede <davidpardede40@gmail.com>
2023-07-24 12:30:31 +0700

    first commit

---
+++ dictionaries/6_1_person.py
@@ -0,0 +1,13 @@
+'''
+Use a dictionary to store information about a person you know. Store their first name, last name, age, and the city. Print each piece of information stored in your dictionary.
+'''
+
+thalia = {'first_name': 'Thalia',
+          'last_name':'Gurning',
+          'age': 25,
+          'city': 'Cilegon'}
+
+print(thalia['first_name'])
+print(thalia['last_name'])
+print(thalia['age'])
+print(thalia['city'])
