255639d8c1e5471a146f88d2e3d4e8822a875234
davidcxpardede <davidpardede40@gmail.com>
2023-07-25 17:25:31 +0700

    User Inputs

---
+++ 7_user_input_and_while_loops/7_1_rental_car.py
@@ -0,0 +1,11 @@
+'''
+Write a program that asks the user what kind of rental car they would like. Print a message about that car, such as "Let me see if I can find you a Subaru."
+'''
+
+motorcycle_type = input("What type of motorcycles would you like to rent? ")
+
+motorcycle_color = input("What color would you like your motorcycle be? ")
+
+
+print("Thank you for your input!")
+print(f"We will check whether a {motorcycle_color} {motorcycle_type} is available and get back to you soon.")
