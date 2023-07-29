771b85dbbf72781a5f741a8efa3cc38fe67ee7c1
davidcxpardede <davidpardede40@gmail.com>
2023-07-29 13:12:59 +0700

    Testing

--- 11_testing/test_cities.py
+++ 11_testing/test_cities.py
@@ -4,3 +4,8 @@ def test_city_country():
     """Test the function city_country()"""
     formatted_city_country = city_country('medan', 'sumatra utara')
     assert formatted_city_country == 'Medan, Sumatra Utara'
+    
+def test_city_country_population():
+    """Test the function city_country if population argument is provided."""
+    formatted_city_country = city_country('medan', 'sumatra utara', 10)
+    assert formatted_city_country == 'Medan, Sumatra Utara - population 10 million'
