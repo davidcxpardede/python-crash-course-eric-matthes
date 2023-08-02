18c3eac6c06ed14bfd2ce356afa7273da441ac72
davidcxpardede <davidpardede40@gmail.com>
2023-08-02 19:43:32 +0700

    Displaying the result in histogram

--- 15_generating_data/die_visual.py
+++ 15_generating_data/die_visual.py
@@ -1,3 +1,5 @@
+import plotly.express as px
+
 from die import Die
 
 # Create a D6.
@@ -6,8 +8,17 @@ die = Die()
 # Make some rolls, and store results in a list.
 results = []
 
-for roll_num in range(100):
+for roll_num in range(1000):
     result = die.roll()
     results.append(result)
-    
-print(results)
+
+# Analyze the results.
+frequencies = []
+poss_results = range(1, die.num_sides+1)
+for value in poss_results:
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# Visualize the results.
+fig = px.bar(x=poss_results, y=frequencies)
+fig.show()
